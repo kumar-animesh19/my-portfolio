4a6a3dc7a768910b6f333035b6ae9e940288b3fb
Kumar Animesh <132490397+kumar-animesh19@users.noreply.github.com>
2024-03-15 15:55:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+let tablinks = document.getElementsByClassName("tab-links");
+let tabcontents = document.getElementsByClassName("tab-contents");
+function opentab(tabname) {
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+// script for side menu for small screen
+
+let sidemenu = document.getElementById("sidemenu");
+function openmenu(){
+    sidemenu.style.right = "0";
+}
+function closemenu(){
+    sidemenu.style.right = "-150px";
+}
+
+// script for google sheet
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyRtCvE9lf0i3wyDHyiXVwNAB0Pz2rPBVJjEQde6-ML33yeQDWBOYUj5EXLsFkk0dE_/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        msg.innerHTML = "Message sent successfully"
+        setTimeout(function(){
+            msg.innerHTML = ""
+        },5000)
+        form.reset()
+        document.getElementsByClassName("btn2").disabled = false;
+    })
+    .catch(error => console.error('Error!', error.message))
+})
